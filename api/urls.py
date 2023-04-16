499fbd4f8cd32babc6ff660c37b7e53699b9dac3
Azamat <azambylbaev@gmail.com>
2023-04-16 17:03:53 +0500

    JWT-Auth

--- api/urls.py
+++ api/urls.py
@@ -11,7 +11,8 @@ router.register('vacancies', views.VacancyViewSet)
 
 urlpatterns = [
     path('companies/<int:pk>/vacancies/', views.CompanyVacancyList.as_view({'get': 'list'})),
-    path('vacancies/top_ten/', views.TopTenVacanciesView.as_view())
+    path('vacancies/top_ten/', views.TopTenVacanciesView.as_view()),
+    # path('vacancies/<int:id>/submit/', )
 ]
 
 urlpatterns += router.urls
